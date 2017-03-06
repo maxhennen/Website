1d252c63a5dfd3de3d02b9d48552cd82ece3a5dc
maxhe <maxhe@DESKTOP-VR2JK4O>
2017-03-06 15:15:00 +0100

    tekst toegevoegd

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +14,30 @@ namespace Website.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Solliciteren()
+        {
+            return View();
+        }
+
+        public ActionResult AfspraakMaken(string onderwerp, string naam, long telefoon, string email, string vraag)
+        {
+            MailMessage mail = new MailMessage(email, "max.hennen@planet.nl");
+            SmtpClient client = new SmtpClient();
+            client.Port = 25;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Host = "smtp.kpnmail.nl";
+            mail.Subject = "this is a test email.";
+            mail.Body = "this is my test email body";
+            client.Send(mail);
+            return RedirectToAction("Contact","Home");
         }
     }
 }
